7908423d42271c8c31b7c05d09c6fb648b74426a
Gustav <gustavmangion@hotmail.com>
2023-06-08 13:45:20 +0400

    Add Book class and testing code for book

--- library.js
+++ library.js
@@ -1,1 +1,24 @@
-console.log("Simple Library!");
+let myLibrary = [];
+
+class Book {
+	constructor(title, author, pages) {
+		this.title = title;
+		this.author = author;
+		this.pages = pages;
+	}
+}
+
+function addBookToLibrary() {
+	myLibrary.push(new Book("Test", "Me", 50));
+	myLibrary.push(new Book("A Book", "You", 55));
+	myLibrary.push(new Book("A Novel", "Us", 90));
+}
+
+function getLibraryBooks() {
+	myLibrary.map((x) => {
+		console.log(x.title);
+	});
+}
+
+addBookToLibrary();
+getLibraryBooks();
