5d178b36aad3709b6b659256fe24b78a96ba401a
Gustav <gustavmangion@hotmail.com>
2023-07-16 11:41:18 +0400

    Alter styling for header and shelf area

--- library.js
+++ library.js
@@ -1,4 +1,5 @@
 let myLibrary = [];
+const shelfArea = document.querySelector(".shelf");
 
 class Book {
 	constructor(title, author, pages) {
@@ -8,17 +9,30 @@ class Book {
 	}
 }
 
-function addBookToLibrary() {
+function addBooksToLibrary() {
 	myLibrary.push(new Book("Test", "Me", 50));
 	myLibrary.push(new Book("A Book", "You", 55));
 	myLibrary.push(new Book("A Novel", "Us", 90));
+	myLibrary.push(new Book("A Guide", "Him", 90));
+	myLibrary.push(new Book("A Diary", "Her", 90));
 }
 
 function getLibraryBooks() {
 	myLibrary.map((x) => {
-		console.log(x.title);
+		const card = document.createElement("div");
+		card.classList.add("card");
+
+		const title = document.createElement("h2");
+		title.textContent = x.title;
+
+		const author = document.createElement("h3");
+		author.textContent = x.author;
+
+		card.appendChild(title);
+		card.appendChild(author);
+		shelfArea.appendChild(card);
 	});
 }
 
-addBookToLibrary();
+addBooksToLibrary();
 getLibraryBooks();
